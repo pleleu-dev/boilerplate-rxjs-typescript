bfde3613e885169c3302cbf066ef9f4be20b0349
pleleu <pleleu.dev@gmail.com>
2022-04-03 17:18:00 +0200

    Add TailwindCSS and aliases

---
+++ src/pages/app/App.tsx
@@ -0,0 +1,26 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React from "react";
+export const App = () => {
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">
+            Rxjs/React Boilerplate
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+          </div>
+          {/* /End replace */}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
